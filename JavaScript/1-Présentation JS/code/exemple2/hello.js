81ea3ac0284511b1d5fd4ed2109992b742453fd8
Mouhamed Kane <60301922+Mouhamed-Kane@users.noreply.github.com>
2025-09-17 13:27:01 +0000

    Cours de JS

---
+++ JavaScript/1-Présentation JS/code/exemple2/hello.js
@@ -0,0 +1,8 @@
+// Une fonction pour afficher un message
+function sayHello() {
+    alert("Hello World!");
+}
+
+
+// Appel de la fonction au clic du bouton
+document.getElementById("myBtn").onclick = sayHello;
---
+++ JavaScript/2-Ou mettre du js/code/exemple4/myScript.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    document.getElementById("titre").innerHTML = "Encore Nouveau titre.";
+  }
